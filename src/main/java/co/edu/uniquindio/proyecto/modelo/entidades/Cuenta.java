e115d677aef98b922f4d08e66f82a913a2519832
AlejandroZasa <az0031456@gmail.com>
2023-09-06 19:34:16 -0500

    Prueba agregando otra entidad

--- src/main/java/co/edu/uniquindio/proyecto/modelo/entidades/Cuenta.java
+++ src/main/java/co/edu/uniquindio/proyecto/modelo/entidades/Cuenta.java
@@ -1,2 +1,28 @@
-package co.edu.uniquindio.proyecto.modelo.entidades;public class Cuenta {
+package co.edu.uniquindio.proyecto.modelo.entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Cuenta implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private int id;
+
+    private String email;
+    private String contraseña;
 }
